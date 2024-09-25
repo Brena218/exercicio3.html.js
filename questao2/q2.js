683fba92cd6cc67bc34562f97e80da27ecf12e92
Brena <fbrena218@gmail.com>
2024-09-25 16:42:51 -0300

    feito

---
+++ questao2/q2.js
@@ -0,0 +1,19 @@
+let valor1 = Number.parseInt(document.getElementById("v1").value);
+let valor2 = number.parseInt(document.getElementById("v2").value);
+let resultado = document.getElementById("resultado");
+let resultado1 = valor1/valor2;
+
+function calculo (){
+while(valor2 === 0){
+    resultado.textContent = (`valor não aceito ${valor2}`)
+}
+
+    resultado.textContent = resultado1;
+}
+
+
+
+/*
+let resultado1= valor1/valor2;
+console.log(resultado);
+/*/
